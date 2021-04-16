2e20ae6e80c2b7c8c411b035993db996045ee1cb
darkkillex <23dottork13>
2021-04-16 18:36:05 +0200

    Refactoring of MockCars class, in order to improve the logic utility of static methods

--- src/main/java/com/example/restservice/CarHandler.java
+++ src/main/java/com/example/restservice/CarHandler.java
@@ -1,5 +1,6 @@
 package com.example.restservice;
 
+import org.mockito.Mock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/restservice/MockCars.java
+++ src/main/java/com/example/restservice/MockCars.java
@@ -5,17 +5,18 @@ import java.util.List;
 
 public class MockCars {
 
-    private static List<Car> listCar = new ArrayList<Car>();
+    private static List<Car> listCar = setUpDataMock();
 
 
-    public static void setUpDataMock(){
-        listCar.add(new Car("123", "Peugeot", "3008"));
-        listCar.add(new Car("456", "Jeep", "Renegade"));
-        listCar.add(new Car("789", "Renault", "Capture"));
+    private static ArrayList<Car> setUpDataMock(){
+        ArrayList<Car> list = new ArrayList<Car>();
+        list.add(new Car("123", "Peugeot", "3008"));
+        list.add(new Car("456", "Jeep", "Renegade"));
+        list.add(new Car("789", "Renault", "Capture"));
+        return list;
     }
 
     public static Car searchCarByPlate(String plate){
-        setUpDataMock();
         for(Car car : listCar) {
             if(car.getCarPlate().equals(plate)) {
                 return car;
