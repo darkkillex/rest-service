37320b083615900bbc84a688305e7c08b1db873e
darkkillex <23dottork13>
2021-04-25 22:58:43 +0200

    Creation of methods to serialize an error in a custom JSON

--- src/main/java/com/example/restservice/ExceptionInterceptor.java
+++ src/main/java/com/example/restservice/ExceptionInterceptor.java
@@ -1,35 +1,31 @@
 package com.example.restservice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 
 
 @ControllerAdvice
 public class ExceptionInterceptor extends RuntimeException {
 
-    @ExceptionHandler(CustomException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = CustomException.class)
     @ResponseBody
-    public ErrorResponse handleAllExceptions(CustomException ex, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage("Car not found");
-        errorResponse.setCodeError("Error 404");
-        return errorResponse;
+    public ResponseEntity<String> handleAllExceptions(CustomException ex) {
+        return errorResponse(ex.getMessage(), ex.getParameter(), ex.getStatusCode());
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public ErrorResponse handleDefaultException(Exception ex) {
-        ErrorResponse response = new ErrorResponse();
-        response.setMessage(ex.getMessage());
-        response.setCodeError("Error 500");
-        return response;
+    private static String createJson(String message, String parameter, HttpStatus httpStatus) {
+        return "{\"message\" : \"" + message + "\"," +
+                "\"parameter\" : \"" + parameter + "\"," +
+                "\"status code\" : \"" + httpStatus  + "\"}";
     }
 
+    private static ResponseEntity<String> errorResponse(String message, String parameter, HttpStatus httpStatus) {
+        String json = createJson(message, parameter, httpStatus);
+        return new ResponseEntity<>(json, httpStatus);
+    }
 
 }
+
