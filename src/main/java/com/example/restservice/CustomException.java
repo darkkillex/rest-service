7aebd0ccfe9daeff11dce50119926a6c18ddf199
darkkillex <23dottork13>
2021-04-25 23:00:01 +0200

    Changed the parameters of CustomException

--- src/main/java/com/example/restservice/CustomException.java
+++ src/main/java/com/example/restservice/CustomException.java
@@ -1,9 +1,32 @@
 package com.example.restservice;
 
 
+import org.springframework.http.HttpStatus;
+
 public class CustomException extends RuntimeException {
 
-    public CustomException() {
-        super();
-    }
+  private final String message;
+  private final String parameter;
+  private final HttpStatus statusCode;
+
+  public CustomException(String message, String parameter, HttpStatus statusCode) {
+    this.message = message;
+    this.parameter = parameter;
+    this.statusCode = statusCode;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  public String getParameter() {
+    return parameter;
+  }
+
+  public HttpStatus getStatusCode() {
+    return statusCode;
+  }
+
+
 }
