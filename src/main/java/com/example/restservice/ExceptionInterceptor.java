3056245667fc9e22eba03459a7cdb0e045899b9d
darkkillex <23dottork13>
2021-04-26 18:12:11 +0200

    Changed the implementation of ExceptionInterceptor class methods to get a serialized response in JSON and not in String

--- src/main/java/com/example/restservice/ExceptionInterceptor.java
+++ src/main/java/com/example/restservice/ExceptionInterceptor.java
@@ -6,25 +6,30 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @ControllerAdvice
-public class ExceptionInterceptor extends RuntimeException {
+public class ExceptionInterceptor {
 
     @ExceptionHandler(value = CustomException.class)
     @ResponseBody
-    public ResponseEntity<String> handleAllExceptions(CustomException ex) {
+    public ResponseEntity<Map<String, String>> handleAllExceptions(CustomException ex) {
         return errorResponse(ex.getMessage(), ex.getParameter(), ex.getStatusCode());
     }
 
-    private static String createJson(String message, String parameter, HttpStatus httpStatus) {
-        return "{\"message\" : \"" + message + "\"," +
-                "\"parameter\" : \"" + parameter + "\"," +
-                "\"status code\" : \"" + httpStatus  + "\"}";
+    private static Map<String, String> createJson(String message, String parameter, HttpStatus httpStatus) {
+        HashMap<String, String> body = new HashMap<>();
+        body.put("message", message);
+        body.put("parameter", parameter);
+        body.put("status_code", httpStatus.toString());
+        return body;
     }
 
-    private static ResponseEntity<String> errorResponse(String message, String parameter, HttpStatus httpStatus) {
-        String json = createJson(message, parameter, httpStatus);
-        return new ResponseEntity<>(json, httpStatus);
+    private static ResponseEntity<Map<String, String>> errorResponse(String message, String parameter, HttpStatus httpStatus) {
+        Map<String, String> json = createJson(message, parameter, httpStatus);
+        return new ResponseEntity<Map<String, String> >(json, httpStatus);
     }
 
 }
