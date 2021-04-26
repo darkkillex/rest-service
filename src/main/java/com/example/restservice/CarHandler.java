6c5e3ea404ea0e0e5a0f34d43f1518c2b32c67b1
darkkillex <23dottork13>
2021-04-26 20:35:04 +0200

    Implemented POST method, with exception control

--- src/main/java/com/example/restservice/CarHandler.java
+++ src/main/java/com/example/restservice/CarHandler.java
@@ -13,7 +13,7 @@ public class CarHandler extends RuntimeException{
     public ResponseEntity<Car> searchCar(@PathVariable String plate){
         Car car = MockCars.findByPlate(plate);
         if(car==null || car.getCarPlate().isEmpty()) {
-            throw new CustomException("value_not_found_GET", "plate", HttpStatus.BAD_REQUEST);
+            throw new CustomException("value_not_found_GET", "plate", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Car>(car,HttpStatus.OK);
     }
@@ -26,18 +26,24 @@ public class CarHandler extends RuntimeException{
     }
 
     @PostMapping(path = "/car", consumes = "application/json", produces = "application/json")
-    public Car createCar(@RequestBody Car car) {
+    public ResponseEntity<Car> createCar(@RequestBody Car car) {
         if (car.getCarPlate()==null || car.getCarPlate().isEmpty()){
             throw new CustomException("null_or_empty_value_POST", "plate", HttpStatus.BAD_REQUEST);
         }
-        return MockCars.saveCar(car);
+        if (car.getCarModel()==null || car.getCarModel().isEmpty()){
+            throw new CustomException("null_or_empty_value_POST", "model", HttpStatus.BAD_REQUEST);
+        }
+        if (car.getCarBrand()==null || car.getCarBrand().isEmpty()){
+            throw new CustomException("null_or_empty_value_POST", "brand", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Car>(MockCars.saveCar(car), HttpStatus.OK);
     }
 
     @DeleteMapping("/car/{plate}")
     public ResponseEntity<Car> removeCar(@PathVariable String plate) {
         Car car = MockCars.removeCarFromList(plate);
         if (car==null || car.getCarPlate().isEmpty()){
-            throw new CustomException("null_or_empty_value_POST", "plate", HttpStatus.BAD_REQUEST);
+            throw new CustomException("null_or_empty_value_POST", "plate", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Car>(car,HttpStatus.OK);
     }
