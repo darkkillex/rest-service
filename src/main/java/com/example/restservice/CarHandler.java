bb7e8405a2022d99138b2a2cfeb08f3aee1def96
darkkillex <23dottork13>
2021-05-02 18:00:29 +0200

    Refactoring of the custom metric (counter) for CRUD operations. Now it is implemented using "Tag" option

--- src/main/java/com/example/restservice/CarHandler.java
+++ src/main/java/com/example/restservice/CarHandler.java
@@ -1,10 +1,12 @@
 package com.example.restservice;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,11 +18,12 @@ public class CarHandler extends RuntimeException {
         this.registry = registry;
     }
 
+
     @GetMapping("/car/{id}")
     public ResponseEntity<Car> searchCar(@PathVariable int id) {
+        addMetricToRequest("GET_SINGLE_CAR");
         Car car = MockCars.findById(id);
         MockCars.checkObjectIsNotNull(car);
-        registry.counter("number_of_single_car_requests_GET").increment();
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
@@ -28,22 +31,23 @@ public class CarHandler extends RuntimeException {
     public List<Car> listCar(@RequestParam(required = false) String plate,
                              @RequestParam(required = false) String model,
                              @RequestParam(required = false) String brand) {
-        registry.counter("number_of_list_car_requests_GET").increment();
+        addMetricToRequest("GET_ALL_CARS");
         return MockCars.getFilteredListCar(plate, model, brand);
     }
 
     @PostMapping(path = "/car", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        addMetricToRequest("POST");
         MockCars.checkValueParameter(car.getCarPlate(), "carPlate");
         MockCars.checkValueParameter(car.getCarModel(), "carModel");
         MockCars.checkValueParameter(car.getCarBrand(), "carBrand");
         MockCars.checkUniquePlate(car.getCarPlate());
-        registry.counter("number_of_requests_POST").increment();
         return new ResponseEntity<Car>(MockCars.saveCar(car), HttpStatus.CREATED);
     }
 
     @PutMapping("/car/{id}")
     public ResponseEntity<Car> updateCar(@RequestBody Car carDetails, @PathVariable int id) {
+        addMetricToRequest("PUT");
         Car car = MockCars.findById(id);
         MockCars.checkObjectIsNotNull(car);
         MockCars.checkValueParameter(carDetails.getCarPlate(), "carPlate");
@@ -53,17 +57,22 @@ public class CarHandler extends RuntimeException {
         car.setCarPlate(carDetails.getCarPlate());
         car.setCarModel(carDetails.getCarModel());
         car.setCarBrand(carDetails.getCarBrand());
-        registry.counter("number_of_requests_PUT").increment();
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
     @DeleteMapping("/car/{id}")
     public ResponseEntity<Car> removeCar(@PathVariable int id) {
+        addMetricToRequest("DELETE");
         Car car = MockCars.removeCarFromList(id);
         MockCars.checkObjectIsNotNull(car);
-        registry.counter("number_of_requests_DELETE").increment();
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
+    public void addMetricToRequest(String tag) {
+        Tag methodTag = Tag.of("method", tag);
+        List<Tag> tags = new ArrayList<>();
+        tags.add(methodTag);
+        registry.counter("number_of_requests",tags).increment();
+    }
 
 }
