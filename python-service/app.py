442ab78324f3c86abb8cfc6bde46e4351a68acde
darkkillex <23dottork13>
2021-05-25 16:10:28 +0200

    Implemented an endpoint to push 10 random car on "/car" endpoint

--- python-service/app.py
+++ python-service/app.py
@@ -1,9 +1,11 @@
+import json
+
+import requests
 from flask import Flask, jsonify
 from faker import Faker
 from faker_vehicle import VehicleProvider
 
 app = Flask(__name__)
-
 
 fake = Faker('it_IT')
 fake.add_provider(VehicleProvider)
@@ -12,13 +14,20 @@ dict_cars = []
 
 
 @app.route('/injectdata')
-def get_car():
+def push_single_car():
     for _ in range(10):
-        car = {'carPlate': fake.license_plate(),
-               'carBrand': fake.vehicle_make(),
-               'carModel': fake.vehicle_model()}
+        car = {
+            'carPlate': fake.license_plate(),
+            'carBrand': fake.vehicle_make(),
+            'carModel': fake.vehicle_model()
+        }
         dict_cars.append(car)
-    return jsonify(dict_cars), 201
+        url = "http://localhost:9090/car"
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        resp = requests.post(url, data=json.dumps(car, sort_keys=False), headers=headers)
+        print(resp.json())
+        print(resp.status_code)
+    return jsonify(dict_cars)
 
 
 if __name__ == '__main__':
