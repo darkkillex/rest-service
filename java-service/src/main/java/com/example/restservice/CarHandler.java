f8d0375ef5fdc122666a9c6f2ff75b01c2a324bf
darkkillex <mastrangelo.nicola@gmail.com>
2021-06-25 16:39:51 +0200

    Added a timer custom metric for CRUD ops

--- java-service/src/main/java/com/example/restservice/CarHandler.java
+++ java-service/src/main/java/com/example/restservice/CarHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Timed
 @RestController
@@ -18,9 +19,12 @@ public class CarHandler extends RuntimeException {
 
     @GetMapping("/car/{id}")
     public ResponseEntity<Car> searchCar(@PathVariable int id) {
+        long start_time = System.currentTimeMillis();
         Car car = MockCars.findById(id);
         UtilityHandler.checkObjectIsNotNull(car);
-        meterRegistry.counter("custom_java_service_single_car_requests_GET").increment();
+        meterRegistry.counter("custom_java_service_single_car_requests_GET_COUNTER").increment();
+        meterRegistry.timer("custom_java_service_single_car_requests_GET_TIMER").
+                record(System.currentTimeMillis() - start_time, TimeUnit.MILLISECONDS);
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
@@ -28,22 +32,29 @@ public class CarHandler extends RuntimeException {
     public List<Car> listCar(@RequestParam(required = false) String plate,
                              @RequestParam(required = false) String model,
                              @RequestParam(required = false) String brand) {
-        meterRegistry.counter("custom_java_service_list_car_requests_GET").increment();
+        long start_time = System.currentTimeMillis();
+        meterRegistry.counter("custom_java_service_list_car_requests_GET_COUNTER").increment();
+        meterRegistry.timer("custom_java_service_list_car_requests_GET_TIMER").
+                record(System.currentTimeMillis() - start_time, TimeUnit.MILLISECONDS);
         return MockCars.getFilteredListCar(plate, model, brand);
     }
 
     @PostMapping(path = "/car", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        long start_time = System.currentTimeMillis();
         UtilityHandler.checkValueParameter(car.getCarPlate(), "carPlate");
         UtilityHandler.checkValueParameter(car.getCarModel(), "carModel");
         UtilityHandler.checkValueParameter(car.getCarBrand(), "carBrand");
         MockCars.checkUniquePlate(car.getCarPlate());
-        meterRegistry.counter("custom_java_service_requests_POST").increment();
+        meterRegistry.counter("custom_java_service_requests_POST_COUNTER").increment();
+        meterRegistry.timer("custom_java_service_requests_POST_TIMER").
+                record(System.currentTimeMillis() - start_time, TimeUnit.MILLISECONDS);
         return new ResponseEntity<Car>(MockCars.saveCar(car), HttpStatus.CREATED);
     }
 
     @PutMapping("/car/{id}")
     public ResponseEntity<Car> updateCar(@RequestBody Car carDetails, @PathVariable int id) {
+        long start_time = System.currentTimeMillis();
         Car car = MockCars.findById(id);
         UtilityHandler.checkObjectIsNotNull(car);
         UtilityHandler.checkValueParameter(carDetails.getCarPlate(), "carPlate");
@@ -53,15 +64,20 @@ public class CarHandler extends RuntimeException {
         car.setCarPlate(carDetails.getCarPlate());
         car.setCarModel(carDetails.getCarModel());
         car.setCarBrand(carDetails.getCarBrand());
-        meterRegistry.counter("custom_java_service_requests_PUT").increment();
+        meterRegistry.counter("custom_java_service_requests_PUT_COUNTER").increment();
+        meterRegistry.timer("custom_java_service_requests_PUT_TIMER").
+                record(System.currentTimeMillis() - start_time, TimeUnit.MILLISECONDS);
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
     @DeleteMapping("/car/{id}")
     public ResponseEntity<Car> removeCar(@PathVariable int id) {
+        long start_time = System.currentTimeMillis();
         Car car = MockCars.removeCarFromList(id);
         UtilityHandler.checkObjectIsNotNull(car);
-        meterRegistry.counter("custom_java_service_requests_DELETE").increment();
+        meterRegistry.counter("custom_java_service_requests_DELETE_COUNTER").increment();
+        meterRegistry.timer("custom_java_service_requests_DELETE_TIMER").
+                record(System.currentTimeMillis() - start_time, TimeUnit.MILLISECONDS);
         return new ResponseEntity<Car>(car, HttpStatus.OK);
     }
 
